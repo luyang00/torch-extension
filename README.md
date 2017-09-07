bd984e63dafc0d82935b1c2e1cedc7fa0ecdc0af
sniklaus <simon.niklaus@outlook.com>
2017-09-07 11:59:49 -0700

    no message

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ This is an example of a CUDA extension for Torch which computes the Hadamard pro
 For the PyTorch version of this example extension, please see: https://github.com/sniklaus/pytorch-extension
 
 To build the extension, run `bash install.bash` and make sure that the `CUDA_HOME` environment variable is set. After succesfully building the extension, run `th test.lua` to test it. Should you receive an error message regarding an invalid device function when making use of the extension, configure the CUDA architecture within `CMakeLists.txt` to something your graphics card supports.
+
+```lua
+require 'torch'
+require 'nn'
+require 'cunn'
+require 'nnex'
+
+torch.setdefaulttensortype('torch.FloatTensor')
+
+local net = nn.HadamardProduct():cuda()
+
+local input1 = torch.rand(64, 3, 128, 128):cuda()
+local input2 = torch.rand(64, 3, 128, 128):cuda()
+
+local output = net:forward({ input1, input2 })
+local expected = torch.cmul(input1, input2)
+
+print(torch.sum(output - expected), '<-- should be 0.0')
+```
--- test.lua
+++ test.lua
@@ -10,36 +10,32 @@ for i = 1, 10 do
 	local input1 = torch.rand(64, 3, 128, 128):cuda()
 	local input2 = torch.rand(64, 3, 128, 128):cuda()
 
+	local output = net:forward({ input1, input2 })
 	local expected = torch.cmul(input1, input2)
 
-	local out = net:forward({ input1, input2 })
+	print(torch.sum(output - expected), '<-- should be 0.0')
 
-	print(torch.sum(out - expected), '<-- should be 0.0')
-
-	net:backward({ input1, input2 }, out)
+	net:backward({ input1, input2 }, output)
 end
 
 print('switching to DataParallelTable')
 
--- the DataParallelTable does not like not having any learnable parameters
--- this example hence adds a SpatialConvolution that does not have any effect
-local dummy = nn.SpatialConvolution(3, 3, 1, 1, 1, 1, 0, 0)
+-- the DataParallelTable does not like not having any learnable parameters, this example hence adds a SpatialConvolution that does not have any effect
+local dummy = nn.SpatialConvolution(3, 3, 1, 1, 1, 1, 0, 0):noBias()
 dummy.weight:zero()
 dummy.weight[{ { 1 }, { 1 }, {}, {} }] = 1.0
 dummy.weight[{ { 2 }, { 2 }, {}, {} }] = 1.0
 dummy.weight[{ { 3 }, { 3 }, {}, {} }] = 1.0
-dummy.bias:zero()
 
 local net = nn.DataParallelTable(1):add(nn.Sequential():add(nn.HadamardProduct()):add(dummy), torch.range(1, cutorch.getDeviceCount()):totable()):cuda()
 for i = 1, 10 do
 	local input1 = torch.rand(64, 3, 128, 128):cuda()
 	local input2 = torch.rand(64, 3, 128, 128):cuda()
 
+	local output = net:forward({ input1, input2 })
 	local expected = torch.cmul(input1, input2)
 
-	local out = net:forward({ input1, input2 })
+	print(torch.sum(output - expected), '<-- should be 0.0')
 
-	print(torch.sum(out - expected), '<-- should be 0.0')
-
-	net:backward({ input1, input2 }, out)
+	net:backward({ input1, input2 }, output)
 end
