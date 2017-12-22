2aedfa96290468191048e5feb62211a53270af00
sniklaus <simon.niklaus@outlook.com>
2017-12-22 14:58:43 -0800

    no message

--- test.lua
+++ test.lua
@@ -2,40 +2,69 @@ require 'torch'
 require 'nn'
 require 'cunn'
 require 'nnex'
+require 'optim'
 
 torch.setdefaulttensortype('torch.FloatTensor')
 
-local net = nn.HadamardProduct():cuda()
-for i = 1, 10 do
-	local input1 = torch.rand(64, 3, 128, 128):cuda()
-	local input2 = torch.rand(64, 3, 128, 128):cuda()
+----------------------------------------------------------
 
-	local output = net:forward({ input1, input2 })
-	local expected = torch.cmul(input1, input2)
+-- the gradcheck requires learnable parameters, this example hence adds a dummy SpatialConvolution that does not have any effect
 
-	print(torch.sum(output - expected), '<-- should be 0.0')
+dummy = nn.SpatialConvolution(3, 3, 1, 1, 1, 1, 0, 0):noBias()
 
-	net:backward({ input1, input2 }, output)
-end
-
-print('switching to DataParallelTable')
-
--- the DataParallelTable does not like not having any learnable parameters, this example hence adds a SpatialConvolution that does not have any effect
-local dummy = nn.SpatialConvolution(3, 3, 1, 1, 1, 1, 0, 0):noBias()
 dummy.weight:zero()
 dummy.weight[{ { 1 }, { 1 }, {}, {} }] = 1.0
 dummy.weight[{ { 2 }, { 2 }, {}, {} }] = 1.0
 dummy.weight[{ { 3 }, { 3 }, {}, {} }] = 1.0
 
-local net = nn.DataParallelTable(1):add(nn.Sequential():add(nn.HadamardProduct()):add(dummy), torch.range(1, cutorch.getDeviceCount()):totable()):cuda()
-for i = 1, 10 do
-	local input1 = torch.rand(64, 3, 128, 128):cuda()
-	local input2 = torch.rand(64, 3, 128, 128):cuda()
+----------------------------------------------------------
+
+local net = nn.Sequential():add(nn.HadamardProduct()):add(dummy):cuda()
+local criterion = nn.AbsCriterion():cuda()
+local params, gradients = net:getParameters()
+for i = 1, 3 do
+	local input1 = torch.rand(2, 3, 8, 8):cuda()
+	local input2 = torch.rand(2, 3, 8, 8):cuda()
+	local truth = torch.rand(2, 3, 8, 8):cuda()
 
 	local output = net:forward({ input1, input2 })
 	local expected = torch.cmul(input1, input2)
 
 	print(torch.sum(output - expected), '<-- should be 0.0')
+	print(optim.checkgrad(function(params)
+		gradients:zero()
 
-	net:backward({ input1, input2 }, output)
+		local output = net:forward({ input1, input2 })
+		local loss = criterion:forward(output, truth)
+
+		net:backward({ input1, input2 }, criterion:backward(output, truth))
+
+		return loss, gradients
+	end, params, 0.001), '<-- should be small')
+end
+
+print('switching to DataParallelTable')
+
+local net = nn.DataParallelTable(1):add(nn.Sequential():add(nn.HadamardProduct()):add(dummy), torch.range(1, cutorch.getDeviceCount()):totable()):cuda()
+local criterion = nn.AbsCriterion():cuda()
+local params, gradients = net:getParameters()
+for i = 1, 3 do
+	local input1 = torch.rand(2, 3, 8, 8):cuda()
+	local input2 = torch.rand(2, 3, 8, 8):cuda()
+	local truth = torch.rand(2, 3, 8, 8):cuda()
+
+	local output = net:forward({ input1, input2 })
+	local expected = torch.cmul(input1, input2)
+
+	print(torch.sum(output - expected), '<-- should be 0.0')
+	print(optim.checkgrad(function(params)
+		gradients:zero()
+
+		local output = net:forward({ input1, input2 })
+		local loss = criterion:forward(output, truth)
+
+		net:backward({ input1, input2 }, criterion:backward(output, truth))
+
+		return loss, gradients
+	end, params, 0.001), '<-- should be small')
 end
